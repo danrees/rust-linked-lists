f3abf6aade6b9b76561334e3417ef74c8f28b914
Daniel Rees <daniel.rees@autodata.net>
2019-11-21 17:01:56 -0500

    finished https://rust-unofficial.github.io/too-many-lists/second-iter-mut.html

--- src/second.rs
+++ src/second.rs
@@ -63,6 +63,10 @@ impl<T> List<T> {
     pub fn iter(&self) -> Iter<T> {
         Iter { next: self.head.as_ref().map(|node| &**node)}
     }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut { next: self.head.as_mut().map(|node| &mut **node)}
+    }
 }
 
 impl<T> Iterator for IntoIter<T> {
@@ -86,6 +90,21 @@ impl<'a, T> Iterator for Iter<'a, T> {
             &node.elem
         })
     }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_mut().map(|node| &mut **node);
+            &mut node.elem
+        })
+    }
+}
+
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
 }
 
 #[cfg(test)]
@@ -170,5 +189,16 @@ mod test {
         assert_eq!(iter.next(), Some(&2));
         assert_eq!(iter.next(), Some(&1));
         assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
     }
 }
